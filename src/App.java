0a6348b59e0e093155e2d74d7e61f2c5f4050e1d
Eric Coe <coeeri001@warren.k12.in.us>
2024-09-26 13:35:39 -0400

    Initial Commitment

--- src/App.java
+++ src/App.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class App {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        LocalDateTime  fakeBirthday = LocalDateTime.of(2000, 8, 11, 3, 11, 0);
+
+        ZoneId timeZone = ZoneId.of("Europe/Paris");
+
+        ZonedDateTime convertedBirthday = ZonedDateTime.of(fakeBirthday, timeZone);
+        System.out.println(convertedBirthday);
+
+    }
+
 }
